fa1b971ddbb60f6263bd1bfa968fd5bc2d461dc0
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-10-15 01:55:02 -0400

    Added Chainlink with prefix command

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const PORT = 8000;
 const express = require('express');
 const cheerio = require('cheerio');
 const axios = require('axios');
+const {
+  Client,
+  GatewayIntentBits,
+  EmbedBuilder,
+  PermissionsBitField,
+  Permissions,
+} = require('discord.js');
+
+require('dotenv').config(); //initialize dotenv
+const prefix = '>';
 
 const app = express();
 
@@ -12,8 +22,8 @@ app.get('/', (req, res) => {
   res.json('Welcome to the 4chan Webscraper API');
 });
 
-const getLink = () => {
-  axios(url).then((response) => {
+const getLink = async () => {
+  await axios(url).then((response) => {
     const html = response.data;
     const $ = cheerio.load(html);
 
@@ -30,5 +40,56 @@ app.get('/link', (req, res) => {
     getLink();
   }, 5000);
 });
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on('ready', () => {
+  console.log('Bot is online');
+
+  client.user.setActivity(`I'm the Coingecko BOT.{type: "WATCHING"}`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  //messages
+
+  const messageArray = message.content.split(' ');
+  const argument = messageArray.slice(1);
+  const cmd = messageArray[0];
+
+  //commands
+  if (command === 'test') {
+    message.channel.send('The bot is currently working properly!');
+  }
+
+  if (command === 'ping') {
+    message.channel.send('Pong');
+  }
+
+  if (command === 'link') {
+    await axios(url).then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      const item = $('body > div.container ');
+      // console.log(item);
+
+      linkPrice.value = $(item).find('.no-wrap').first().text();
+      console.log(`Current price of link is ${linkPrice.value}`);
+    });
+    message.channel.send(`Current price of Chainlink is ${linkPrice.value}`);
+  }
+});
+
+client.login(process.env.CLIENT_TOKEN);
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
