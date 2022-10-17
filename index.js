5d546684ec4adb077a0a9257205f54f69fdc3a53
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-10-16 23:42:44 -0400

    complete base requirements

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const prefix = '>';
 
 const app = express();
 
-const url = 'https://www.coingecko.com/en/coins/chainlink';
+const url = 'https://www.coingecko.com/en/coins/';
 const linkPrice = {};
 
 app.get('/', (req, res) => {
@@ -52,7 +52,7 @@ const client = new Client({
 client.on('ready', () => {
   console.log('Bot is online');
 
-  client.user.setActivity(`I'm the Coingecko BOT.{type: "WATCHING"}`);
+  client.user.setActivity(`WAITING FOR COMMANDS`);
 });
 
 client.on('messageCreate', async (message) => {
@@ -66,6 +66,10 @@ client.on('messageCreate', async (message) => {
   const argument = messageArray.slice(1);
   const cmd = messageArray[0];
 
+  var newNumber;
+  var baseNumber;
+  var originalNumber;
+
   //commands
   if (command === 'test') {
     message.channel.send('The bot is currently working properly!');
@@ -75,18 +79,67 @@ client.on('messageCreate', async (message) => {
     message.channel.send('Pong');
   }
 
-  if (command === 'link') {
-    await axios(url).then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
+  if (command) {
+    axios(`${url}${command}`)
+      .then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
 
-      const item = $('body > div.container ');
-      // console.log(item);
+        const item = $('body > div.container ');
+        // console.log(item);
 
-      linkPrice.value = $(item).find('.no-wrap').first().text();
-      console.log(`Current price of link is ${linkPrice.value}`);
-    });
-    message.channel.send(`Current price of Chainlink is ${linkPrice.value}`);
+        linkPrice.value = $(item).find('.no-wrap').first().text();
+        baseNumber = Number(linkPrice.value.replace(/[^0-9.-]+/g, ''));
+        originalNumber = baseNumber;
+        console.log(`Setting BASE ${command} price: ${baseNumber}`);
+        message.channel.send(
+          `Setting BASE ${
+            command.charAt(0).toUpperCase() + command.substring(1)
+          } price: $${baseNumber}. Checking every 30 seconds. Will notify IF price changes.`
+        );
+      })
+      .catch(function (error) {
+        console.log(error.response);
+        message.channel.send(`Price Set Error`);
+      });
+    setInterval(() => {
+      axios(`${url}${command}`)
+        .then((response) => {
+          const html = response.data;
+          const $ = cheerio.load(html);
+
+          const item = $('body > div.container ');
+          // console.log(item);
+
+          linkPrice.value = $(item).find('.no-wrap').first().text();
+          newNumber = Number(linkPrice.value.replace(/[^0-9.-]+/g, ''));
+          console.log(`Price of ${command} is $${newNumber}`);
+        })
+        .then(() => {
+          if (newNumber != baseNumber) {
+            console.log(
+              `${
+                command.charAt(0).toUpperCase() + command.substring(1)
+              } Price Change ALERT! The Price of ${command} has changed to $${newNumber}.`
+            );
+            message.channel.send(
+              `${
+                command.charAt(0).toUpperCase() + command.substring(1)
+              } Price Change ALERT! The Price of ${command} has changed to $${newNumber}.`
+            );
+
+            baseNumber = newNumber;
+            console.log(`BASE price UPDATED to $${baseNumber}`);
+            message.channel.send(
+              `BASE Price UPDATED to: $${baseNumber}. Original Price: $${originalNumber}.`
+            );
+          }
+        })
+        .catch(function (error) {
+          console.log(error.response);
+          message.channel.send(`Interval Error: Try another name`);
+        });
+    }, 30000);
   }
 });
 
